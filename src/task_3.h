b3cdf67a691a1940d3e5e93e0e6dedfa1dfe311b
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-16 22:54:14 +0500

    Update task_3.h

--- src/task_3.h
+++ src/task_3.h
@@ -1,1 +1,11 @@
-/
+void rearrangeSoldiers(vector<string>& soldiers) {
+    const size_t soldierCount = soldiers.size();
+
+    for (size_t i = 0; i < soldierCount - 1; i += 2) {
+        string temp = soldiers[i];
+        soldiers[i] = soldiers[i + 1];
+        soldiers[i + 1] = temp;
+    }
+    for (const string& soldier : soldiers) {
+        cout << soldier << " ";
+    }
